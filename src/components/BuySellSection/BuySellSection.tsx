bfee512048f03916314c90c5fff3dea5e3b82202
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-08-30 15:52:07 +0100

    added buysell design

--- src/components/BuySellSection/BuySellSection.tsx
+++ src/components/BuySellSection/BuySellSection.tsx
@@ -44,7 +44,7 @@ import {
                     <div className="display-flex-withspace"> 
                         <div className="display-flex-withspace">
                             <img src="./vectors/etherum.svg" />
-                            <p>ETH</p>
+                            <p className="eth-text">ETH</p>
                         </div>
                         <div>
                         <p>We Buy @ <span className="bold-currency">N345/$ </span></p>
@@ -60,16 +60,16 @@ import {
                                 <p>Bank Transfer</p>
                                 <p>NGN</p>
                             </div>
-                            <p>#3452030300303.24/$</p>
+                            <p className="bold-currency">#3452030300303.24/$</p>
                         </div>
                         
                         <div className="display-flex-withspace"> 
                             <div className="display-flex">
                                     <img className="btc-logo" src="./vectors/bitcoin.svg" />
-                                    <p>Bitcoin</p>
+                                    <p className="btc-resrv-text">Bitcoin</p>
                             </div>
                             <div className="display-flex">
-                                <p>399.98</p>
+                                <p className="bold-currency">399.98</p>
                                 <p>BTC</p>
                             </div>
                         </div>
@@ -77,10 +77,10 @@ import {
                         <div className="display-flex-withspace"> 
                             <div className="display-flex-withspace">
                                 <img src="./vectors/etherum.svg" />
-                                <p>ETH</p>
+                                <p className="eth-resrv-text">Ethereum</p>
                             </div>
                             <div className="display-flex">
-                                <p>1000.00</p>
+                                <p className="bold-currency">1000.00</p>
                                 <p>ETH</p>
                             </div>
                         </div>
