fb4d2c7a4ca96cf7e08add84f71343e864642dc5
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-29 21:01:08 +0100

    feature: added modal component

---
+++ src/components/Modal/ModalComponent.tsx
@@ -0,0 +1,16 @@
+function ModalComponent(){
+    return(
+        <div className="modal-container">
+            <div className="modal show-modal">
+                <div className="modal-content">
+                    <span className="close-button" onClick={()=>{}}>x</span>
+                    <div className="modal-subcontents">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ModalComponent
