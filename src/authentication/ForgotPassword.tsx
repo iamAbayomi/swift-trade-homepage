10c923508a1c2a17ffceff09a07ced95f16e15f6
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-29 22:24:18 +0100

    feature: added method to sendlink

--- src/authentication/ForgotPassword.tsx
+++ src/authentication/ForgotPassword.tsx
@@ -1,25 +1,48 @@
+import { ChangeEvent, useRef, useState } from "react"
+import SimpleReactValidator from "simple-react-validator"
 import styled from "styled-components"
+import { useCustomAxios } from "../classes/Utilities"
 import CustomizeButton from "../components/ui-components/buttons/CustomizeButton"
 import InputField from "../components/ui-components/InputField"
 
 
 function ForgotPassword(){
+    const [email, setEmail] = useState("")
+    const simpleValidator = useRef( new SimpleReactValidator())
+
+    function onEmailChanged(event: ChangeEvent<HTMLInputElement>){
+        setEmail(event.target.value)
+    }
+
+    async function sendResetLink(){
+        let body = {email}
+        if(simpleValidator.current.allValid()){
+            const {response, error} = await useCustomAxios('POST', 'user/password/forgot', body)
+        }
+        else{
+            simpleValidator.current.showMessages()
+        }
+    }
+
+
     return(
         <Contents>
             <HeadingText className="light-heading">Please enter your email address to reset your password</HeadingText>
             <InputField 
                 type={"email"} 
                 placeholder={"Email"}              
-                // value={""}
-                onChange={()=> {}}
+                value={email}
+                onChange={onEmailChanged}
             />
+            {simpleValidator.current.message('email', email, 'required', {className: 'error-message'})}
             <CustomizeButton 
                 width={"150px"} 
                 height={"40px"} 
                 color={"white"} 
                 backgroundColor={"#010066"} 
                 buttonText={"Send"}
-                margin="20px 10px 10px 0px"    
+                margin="20px 10px 10px 0px"
+                onClick={sendResetLink}
             />
         </Contents>
     )
