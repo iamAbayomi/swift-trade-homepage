9868da8a0c4fb3ae8a334a70f094b88a2c283ff7
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-29 21:48:11 +0100

    feature: added button and styled the components

--- src/authentication/ForgotPassword.tsx
+++ src/authentication/ForgotPassword.tsx
@@ -1,17 +1,32 @@
+import styled from "styled-components"
+import CustomizeButton from "../components/ui-components/buttons/CustomizeButton"
 import InputField from "../components/ui-components/InputField"
+
 
 function ForgotPassword(){
     return(
-        <div>
-            <p>Please enter your email to reset your password</p>
+        <Contents>
+            <p className="light-heading">Please enter your email address to reset your password</p>
             <InputField 
                 type={"email"} 
                 placeholder={"Email"}              
                 value={""}
                 onChange={()=> {}}
             />
-        </div>
+            <CustomizeButton 
+                width={"150px"} 
+                height={"40px"} 
+                color={"white"} 
+                backgroundColor={"#010066"} 
+                buttonText={"Send"}
+                margin="20px 10px 10px 0px"    
+            />
+        </Contents>
     )
 }
 
 export default ForgotPassword
+
+const Contents = styled.div`
+    margin: 20px 20px;
+`
