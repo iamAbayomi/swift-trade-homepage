2ffc35f09d00ec357cd3230b8339e0b403779551
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-29 21:48:29 +0100

    feature: passed the components as props

--- src/components/Modal/ModalComponent.tsx
+++ src/components/Modal/ModalComponent.tsx
@@ -1,11 +1,23 @@
-function ModalComponent(){
+import { useState } from 'react'
+import '../ModalForm/ModalClass.css'
+
+type props = {
+     modalContent: any
+}
+
+const ModalComponent: React.FC<props> = (props)=>{
+    const [showModal, setShowModal ] = useState(true)
+    function closeModal(){
+        let tempModal = showModal
+        setShowModal(!tempModal)
+    }
     return(
         <div className="modal-container">
-            <div className="modal show-modal">
+            <div className={`modal ${showModal ? "show-modal" : ""}`}>
                 <div className="modal-content">
-                    <span className="close-button" onClick={()=>{}}>x</span>
+                    <span className="close-button" onClick={closeModal}>x</span>
                     <div className="modal-subcontents">
-
+                        {props.modalContent}
                     </div>
                 </div>
             </div>
