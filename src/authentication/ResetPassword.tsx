030450627cd69a0876194452ac77fbb36f630882
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-29 21:00:51 +0100

    feature: added function to reset password

--- src/authentication/ResetPassword.tsx
+++ src/authentication/ResetPassword.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import InputField from '../components/ui-components/InputField';
 
 
 function Resetpassword(){
-
+    return(
+        <div>
+            <p>Please enter the reset code and your password</p>
+            <InputField
+                type={""}
+                placeholder={""}
+                value={""}
+                onChange={()=>{}}
+            />
+            <InputField
+                type={""}
+                placeholder={""}
+                value={""}
+                onChange={()=>{}}
+            />
+        </div>
+    )
 }
 
 export default Resetpassword;
